6b95340e8c81fb2f799b62ea5200c29e1acba825
pierre trouve <pierre.trouve@gmail.com>
2018-09-01 00:11:06 +0200

    Add Enemy Manager
    Add Enemy Manager + Prefab enemy

---
+++ Assets/Scripts/EnemyAbstract.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyAbstract : MonoBehaviour {
+
+    public GameObject enemyManager;
+
+    EnemyManager enemyManagerScript;
+
+    void Start() {
+        enemyManagerScript = enemyManager.GetComponent<EnemyManager>();
+        enemyManagerScript.AddEnemy(this);
+    }
+
+    public abstract void Init();
+
+    public abstract void Handle();
+}
